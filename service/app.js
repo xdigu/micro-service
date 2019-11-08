0820e61a41e27227293d2a270d13d126a405ea10
Rodrigo <rodrigo@MacBook-Pro-de-dev2-3.local>
2019-11-08 17:46:51 -0300

    adjust queue name

--- service/app.js
+++ service/app.js
@@ -1,9 +1,9 @@
 const amqp = require('amqplib/callback_api');
 const { User } = require('../models');
+const queue = 'user';
 
 amqp.connect('amqp://localhost:5672', function (err, conn) {
     conn.createChannel(function (err, ch) {
-        var queue = 'user';
 
         ch.assertQueue(queue, { durable: false });
         ch.prefetch(1);
